21e528f7495722f0d1a99337c9509b320dd7408a
GLiccardo <peppeliccardo@gmail.com>
2023-09-15 19:32:04 +0200

    [PROJECT] Create DI AppModule

---
+++ app/src/main/java/it/fantacalcio/sample/core/constants/Constants.kt
@@ -0,0 +1,22 @@
+package it.fantacalcio.sample.core.constants
+
+object Constants {
+
+    /*
+     * Generics
+     */
+    const val NO_VALUE = -1
+    const val EMPTY_STRING = ""
+    const val SPACE_STRING = " "
+    const val DASH_STRING = "-"
+    const val COMMA_STRING = ","
+    const val DOT_STRING = "."
+    const val DOUBLE_DOTS_STRING = ":"
+
+
+    /*
+     * Networking Constants
+     */
+    const val BASE_URL = "https://content.fantacalcio.it/test/"
+
+}
---
+++ app/src/main/java/it/fantacalcio/sample/core/di/AppModule.kt
@@ -0,0 +1,73 @@
+package it.fantacalcio.sample.core.di
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import it.fantacalcio.sample.FantacalcioSampleApplication
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context): FantacalcioSampleApplication {
+        return app as FantacalcioSampleApplication
+    }
+
+    @Provides
+    @Singleton
+    fun provideContext(application: FantacalcioSampleApplication): Context {
+        return application.applicationContext
+    }
+
+    /*
+     * Data Sources
+     */
+
+    @Provides
+    @Singleton
+    fun provideApiInterface(app: Application): ApiInterface {
+        return ApiClient.getRetrofitApi(app)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application): DatabaseClient {
+        return Room.databaseBuilder(
+            app,
+            DatabaseClient::class.java,
+            DatabaseClient.DATABASE_NAME
+        ).build()
+    }
+
+    /*
+     * Repository
+     */
+
+    @Provides
+    @Singleton
+    fun providePlayersRepository(
+        api: ApiInterface,
+        db: DatabaseClient
+    ): PlayersRepository {
+        return PostsRepositoryImpl(api, db.dao)
+    }
+
+    /*
+     * Use Cases
+     */
+
+    @Provides
+    @Singleton
+    fun provideGetPlayersUseCase(repository: PlayersRepository): GetPlayersUseCase {
+        return GetPostsUseCase(repository)
+    }
+
+}
