4a2fc2c93fe25f355aa40a069ab227ad09f1c87e
GLiccardo <peppeliccardo@gmail.com>
2023-09-15 19:47:05 +0200

    [PROJECT] Create BaseActivity

---
+++ app/src/main/java/it/fantacalcio/sample/core/ui/base/BaseActivity.kt
@@ -0,0 +1,33 @@
+package it.fantacalcio.sample.core.ui.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
+
+abstract class BaseActivity<VM : AndroidViewModel, VB : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int,
+    val viewModelClass: Class<VM>
+) : AppCompatActivity() {
+
+    lateinit var viewModel: VM
+    lateinit var binding : VB
+
+    open fun doInOnCreate() {}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        init()
+        doInOnCreate()
+    }
+
+    private fun init() {
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+        viewModel = ViewModelProvider(this)[viewModelClass]
+    }
+
+}
