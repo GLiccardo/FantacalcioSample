28b35a22574c153160c830d5966bac973b73babe
GLiccardo <peppeliccardo@gmail.com>
2023-09-18 11:03:11 +0200

    [FEAT PREFERRED] Create uiState

---
+++ app/src/main/java/it/fantacalcio/sample/feature_list/presentation/preferred_list/PreferredListState.kt
@@ -0,0 +1,10 @@
+package it.fantacalcio.sample.feature_list.presentation.preferred_list
+
+import it.fantacalcio.sample.core.constants.Constants.EMPTY_STRING
+import it.fantacalcio.sample.feature_list.domain.model.PlayerModel
+
+data class PreferredListState(
+    val isLoading: Boolean = false,
+    val error: String = EMPTY_STRING,
+    val playersList: List<PlayerModel> = emptyList()
+)
